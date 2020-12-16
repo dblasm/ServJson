d2f92b95f12d618d877223530ae611b3385a1ae0
alexandru <alexandru.duma@grupocmc.es>
2020-12-16 12:47:45 +0100

    alex-> cambios en Conversiones y Gestion_TResultado

--- ServicioJson/src/main/java/com/cmc/hibernate/modelo/Gestion_TResultado.java
+++ ServicioJson/src/main/java/com/cmc/hibernate/modelo/Gestion_TResultado.java
@@ -50,18 +50,23 @@ public class Gestion_TResultado implements IGestion_TObjeto {
 	public boolean cargarHistorico(ObjOrigen objeto,Diccionario diccionario) {
 		try  {
 			
-			List<TagDictionary> tagNames = diccionario.getDiccionario().get(objeto.getIp());
-			
-			TResultado resultado = new TResultado();
+			List<TagDictionary> tagNames = diccionario.getDiccionario().get(objeto.getIp());			
 			List<TResultado> resultados = new ArrayList<TResultado>();
 			
 			for (Dato o : objeto.getDatos()) {
+				
+				TResultado resultado = new TResultado();
 				resultado.setFecha(Conversiones.toTimestamp(objeto.getFecha()));
 				resultado.setValor(Conversiones.toFloat(o.getValor()));
 				resultado.setTagName(tagNames.stream().filter(x -> o.getId().equals(x.getIdPlc())).map(TagDictionary::getTagname)
 						.findAny().orElse(""));
-				resultados.add(resultado);
-				resultado=new TResultado();
+				if (resultado.getFecha()== null || resultado.getValor() == null || resultado.getTagName() == ("")) {
+					
+					log.info("El objeto : " + o.toString() + "no se ha insertado en la base de datos");
+				}
+				else {					
+					resultados.add(resultado);	
+				}
 			}
 			// Save de los resultados
 			tResultado_dao.cargarResultados(resultados);
--- ServicioJson/src/main/java/com/cmc/util/Conversiones.java
+++ ServicioJson/src/main/java/com/cmc/util/Conversiones.java
@@ -6,11 +6,26 @@ public class Conversiones {
 	
 	// Conversión de String a Timestamp
 	public static Timestamp toTimestamp(String fecha) {
+		
+		//Timestamp format must be yyyy-mm-dd hh:mm:ss[.fffffffff]
+		Boolean format = fecha.matches("20[0-9]{2}-(0[1-9]{1}||(1[0-2]{1}))-(0[1-9]{1}||(1[1-9]{1})||(2[1-9]{1})||(3[0-1]{1})) [0-9]{2}:[0-9]{2}:[0-9]{2}(.[0-9])*");
+		
+		if ((fecha == null) || (!format)) {
+			return null;
+		}		
 		return Timestamp.valueOf(fecha);
 	}
 	
 	//Conversión de String a Float
 	public static Float toFloat(String valor) {
-		return Float.parseFloat(valor);
+		
+		//Float (+||-)ffff*.ffff*
+		Boolean format = valor.matches("(\\+||-)[0-9]+\\.[0-9]*");
+				
+				if ((valor == null) || (!format)) {
+					return null;
+				}		
+				return Float.parseFloat(valor);
+		
 	}
 }
