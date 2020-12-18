8cbe6019f6acfee7fdefd8e710b71f1467b5d16a
alexandru <alexandru.duma@grupocmc.es>
2020-12-18 14:49:50 +0100

    ampliación de diccionario y tobjeto

--- ServicioJson/src/main/java/com/cmc/objetos/Diccionario.java
+++ ServicioJson/src/main/java/com/cmc/objetos/Diccionario.java
@@ -36,7 +36,8 @@ public class Diccionario {
 					if (!dicc.containsKey(item.getIp())) {
 						dicc.put(item.getIp(), new ArrayList<TagDictionary>());
 					}
-					dicc.get(item.getIp()).add(new TagDictionary(item.getTagName(), item.getIdValor()));
+					dicc.get(item.getIp()).add(new TagDictionary(item.getTagName(), item.getIdPlc(),item.getEscalado(),item.isEsEscalado(),item.isAgregado(),item.getTiempoAgregado()));
+					
 				}
 	
 				Traza_Log.registro(" ##### Diccionario generado:" + dicc.toString() + " ######");
--- ServicioJson/src/main/java/com/cmc/objetos/TagDictionary.java
+++ ServicioJson/src/main/java/com/cmc/objetos/TagDictionary.java
@@ -6,16 +6,57 @@ public class TagDictionary {
 
 	private String tagname;
 	private String idPlc;
-	
-	public TagDictionary(String tagname, String idPlc) {
-		super();
-		this.tagname = tagname;
-		this.idPlc = idPlc;
-	}	
+	private float escalado;
+	private boolean esEscalado;
+	private boolean agregado;
+	private int tiempoAgregado;
 	
 	public TagDictionary() {
 		super();
 		
+	}
+	
+	public TagDictionary(String tagname, String idPlc, float escalado, boolean esEscalado, boolean agregado,
+			int tiempoAgregado) {
+		super();
+		this.tagname = tagname;
+		this.idPlc = idPlc;
+		this.escalado = escalado;
+		this.esEscalado = esEscalado;
+		this.agregado = agregado;
+		this.tiempoAgregado = tiempoAgregado;
+	}
+
+	public float getEscalado() {
+		return escalado;
+	}
+
+	public void setEscalado(float escalado) {
+		this.escalado = escalado;
+	}
+
+	public boolean isEsEscalado() {
+		return esEscalado;
+	}
+
+	public void setEsEscalado(boolean esEscalado) {
+		this.esEscalado = esEscalado;
+	}
+
+	public boolean isAgregado() {
+		return agregado;
+	}
+
+	public void setAgregado(boolean agregado) {
+		this.agregado = agregado;
+	}
+
+	public int getTiempoAgregado() {
+		return tiempoAgregado;
+	}
+
+	public void setTiempoAgregado(int tiempoAgregado) {
+		this.tiempoAgregado = tiempoAgregado;
 	}
 	
 	public String getTagname() {
